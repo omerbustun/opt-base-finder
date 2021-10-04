0ee8673a12bccd57b9993a72b0187ad5267e2787
Ömer B. Üstün <omerberatustun@gmail.com>
2021-10-05 01:24:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 import numpy as np
 
-area = np.zeros((10,10)) #creating a 10 by 10 area matrix and populating it with zeros
+H = 10 #height of the area matrix
+W = 10 #width of the area matrix
+U = 10 #number of users in the area
 
-area.flat[np.random.choice(10 * 10, 10, replace=False)] = 1 #flattens the matrix to an array, randomly selects 10 values and changes them to 1
+area = np.zeros((H,W)) #creates a 10 by 10 area matrix and populates it with zeros
+
+area.flat[np.random.choice(H * W, U, replace=False)] = 1 #flattens the matrix to an array, randomly selects 10 values and changes them to 1
 
 print(area)

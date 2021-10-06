cbc357b5b644103790e77226052ccdeec32fcb35
Ömer B. Üstün <omerberatustun@gmail.com>
2021-10-06 18:14:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ W = 10 #width of the area matrix
 U = 10 #number of users in the area
 
 area = np.zeros((H,W)) #creates a 10 by 10 area matrix and populates it with zeros
+print(area)
 
 area.flat[np.random.choice(H * W, U, replace=False)] = 1 #flattens the matrix to an array, randomly selects 10 values and changes them to 1
-
 print(area)

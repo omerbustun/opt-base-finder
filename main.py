530610f80fc907701a5d683ce237310ef2e0d4b1
Ömer B. Üstün <omerberatustun@gmail.com>
2021-11-05 22:40:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 import numpy as np
 
-H = 10 #height of the area matrix
-W = 10 #width of the area matrix
-U = 10 #number of users in the area
+H = 10 #int(input("Enter height: "))
+W = 10 #int(input("Enter width: "))
+U = 10 #int(input("Enter number of users: "))
 
-area = np.zeros((H,W)) #creates a 10 by 10 area matrix and populates it with zeros
-print(area)
+area = np.zeros((H ,W)) #creates a 10 by 10 area matrix and populates it with zeros
 
 area.flat[np.random.choice(H * W, U, replace=False)] = 1 #flattens the matrix to an array, randomly selects 10 values and changes them to 1
 print(area)
+
+ones = np.where(area ==1)
+listOfIndices = list(zip(ones[0], ones[1]))
+distances = []
+energyCost = []
+
+for indice in listOfIndices:
+   distances.append(np.sqrt(((abs(5 - indice[0]))**2) + ((abs(5 - indice[1]))**2)))
+
+for distance in distances:
+   energyCost.append(distance**2)
+
+print("Total energy consumption when the base tower is in the middle: ", sum(energyCost))

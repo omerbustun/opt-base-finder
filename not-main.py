3c7af30caf905b74c70b9ab1bc0637d0e9bc9662
Ömer B. Üstün <omerberatustun@gmail.com>
2021-11-05 22:39:07 +0300

    Update not-main.py

--- not-main.py
+++ not-main.py
@@ -7,18 +7,20 @@ U = 10 #int(input("Enter number of users: "))
 
 area = np.zeros((H ,W)) #creates a 10 by 10 area matrix and populates it with zeros
 
-area.flat[np.random.choice(H * W, U, replace=False)] = 1 #flattens the matrix to an      array, randomly selects 10 values and changes them to 1
+area.flat[np.random.choice(H * W, U, replace=False)] = 1 #flattens the matrix to an array, randomly selects 10 values and changes them to 1
 print(area)
 
-ones = np.where(area ==1)
+ones = np.where(area == 1)
 listOfIndices = list(zip(ones[0], ones[1]))
 distances = []
 energyCost = []
 
 for indice in listOfIndices:
-   distances.append(np.sqrt(((abs(5 - indice[0]))**2) + ((abs(5 - indice[1]))**2)))
+   distances.append(np.sqrt(((abs(H//2 - indice[0]))**2) + ((abs(W//2 - indice[1]))**2)))
 
 for distance in distances:
    energyCost.append(distance**2)
+
+print(listOfIndices)
 
 print("Total energy consumption when the base tower is in the middle: ", sum(energyCost))

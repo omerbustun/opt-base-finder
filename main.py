0559248dc87d251efadcc7c8699940b5902e86da
Ömer B. Üstün <omerberatustun@gmail.com>
2021-11-11 00:47:05 +0300

    Final commit

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python
 import numpy as np
 
-H = 10 #int(input("Enter height: "))
-W = 10 #int(input("Enter width: "))
-U = 10 #int(input("Enter number of users: "))
+H = int(input("Enter height: "))
+W = int(input("Enter width: "))
+U = int(input("Enter number of users: "))
 
 area = np.zeros((H ,W)) #creates a 10 by 10 area matrix and populates it with zeros
 
 area.flat[np.random.choice(H * W, U, replace=False)] = 1 #flattens the matrix to an array, randomly selects 10 values and changes them to 1
 print(area)
 
-ones = np.where(area ==1)
+ones = np.where(area == 1)
 listOfIndices = list(zip(ones[0], ones[1]))
 distances = []
-energyCost = []
+energyCostMid = []
+energyCostOpt = []
+energyCostOptLocal = []
+class costData:
+   def __init__(self, x, y, cost):
+      self.x = x
+      self.y = y
+      self.cost = cost
 
 for indice in listOfIndices:
-   distances.append(np.sqrt(((abs(5 - indice[0]))**2) + ((abs(5 - indice[1]))**2)))
+   distances.append(np.sqrt(((abs(H//2 - indice[0]))**2) + ((abs(W//2 - indice[1]))**2)))
 
 for distance in distances:
-   energyCost.append(distance**2)
+   energyCostMid.append(distance**2)
 
-print("Total energy consumption when the base tower is in the middle: ", sum(energyCost))
+for i in range(H):
+   for j in range(W):
+      energyCostOptLocal.clear()
+      for indice in listOfIndices:
+         energyCostOptLocal.append(np.sqrt(((abs(i - indice[0]))**2) + ((abs(j - indice[1]))**2)))
+      energyCostOpt.append(costData(i, j, sum(energyCostOptLocal)))
+
+energyCostOpt.sort(key=lambda x: x.cost)
+
+print(f"Total energy consumption when the base tower is in the middle: {sum(energyCostMid)}")
+print(f"Optimum coordinates for optimum energy consumption: X: {energyCostOpt[0].x} Y: {energyCostOpt[0].y}")
+print(f"Total energy consumption when the base tower is in the optimum coordinates: {energyCostOpt[0].cost}")
